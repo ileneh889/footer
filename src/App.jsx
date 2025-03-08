77a500d0658c2c664aedde05b5aab282982e158e
Ilene <ileneh889@yahoo.com>
2025-03-08 17:14:11 +0800

    final touch

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 
 // components
 import Footer from "./components/Footer.jsx"
-import Button from "./components/shared/Button.jsx"
 
 export default function App() {
   return (
--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,8 +1,34 @@
+// general
+import React, { useEffect, useRef } from 'react';
+
+// css
 import styles from "../../styles/Button.module.css";
 
+
 export default function Button({ content = "button" }) {
+  const buttonRef = useRef(null);
+
+  // fistload animation control and un-hover effect
+  useEffect(() => {
+    const button = buttonRef.current;
+
+    const handleMouseEnter = () => {
+      if (!button.classList.contains(styles.animated)) {
+        button.classList.add(styles.animated);
+      }
+    };
+
+    // 添加事件監聽器
+    button.addEventListener("mouseenter", handleMouseEnter);
+
+    // 清理函數 - 移除事件監聽器
+    return () => {
+      button.removeEventListener("mouseenter", handleMouseEnter);
+    };
+  }, []);
+
   return (
-    <div className={styles.btnOut}>
+    <div className={styles.btnOut} ref={buttonRef}>
       <div className={styles.btnIn}>
         <p className={styles.content}>{content}</p>
         <p className={styles.overlay}>{content}</p>
